e9173bc7673daa42bbc5fd7b2f0f1c489dc66283
brunocampos210@gmail.com <brunocampos210@gmail.com>
2023-02-22 11:37:25 -0300

    Organização de pastas

--- src/components/BarraDeSelecaoDeVeuParaOrcamento/index.tsx
+++ src/components/BarraDeSelecaoDeVeuParaOrcamento/index.tsx
@@ -1,6 +1,8 @@
-import BotaoAdicionar from "components/BotaoAdicionar";
-import BotaoPesquisar from "components/BotaoPesquisar";
+import BotaoAdicionar from "components/Botoes/BotaoAdicionar";
+import BotaoPesquisar from "components/Botoes/BotaoPesquisar";
+import DivFlutuante from "components/DivFlutuante";
 import Input from "components/Input";
+import { Link } from "react-router-dom";
 
 import style from "./BarraDeSelecaoDeVeuParaOrcamento.module.scss";
 
@@ -9,12 +11,22 @@ export default function BarraDeSelecaoDeVeuParaOrcamento(){
         <div className={style.Barra}>
             <span className={style.Barra__TextoVeu}>Véu:</span>
             <Input placeHolder="Selecione o Véu" disabled/>
-            <BotaoPesquisar>
+            <BotaoPesquisar {onclick(()=>ExibirDivFlutuante())}>
                 Selecionar Véu
             </BotaoPesquisar>
-            <BotaoAdicionar>
-                Material
-            </BotaoAdicionar>
+            <Link to="">
+                <BotaoAdicionar>
+                    Material
+                </BotaoAdicionar>
+            </Link>
+        </div>
+    );
+}
+
+function ExibirDivFlutuante(){
+    console.log("te");
+    return(
+        <div>
         </div>
     );
 }
